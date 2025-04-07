7e182f35c2392e9d17e8fd0a83904e57c03ac35e
Lakshmi Deepak <lakshmi.deepak3@gmail.com>
2025-04-08 02:43:25 +0530

    🔥 Remove legacy GeneratedPluginRegistrant causing embedding v1 issues

--- android_backup/android/app/src/main/kotlin/com/example/milk_delivery_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.milk_delivery_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android_backup/app/src/main/kotlin/com/example/milk_delivery_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.milk_delivery_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
